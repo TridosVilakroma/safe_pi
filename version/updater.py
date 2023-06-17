aec7f54f3c7d7c379e41cb751065489ce72d6919
CalebStock <calebstock91@gmail.com>
2023-06-17 10:03:52 -0500

    updater ready for testing

--- version/updater.py
+++ version/updater.py
@@ -1,4 +1,4 @@
-import hashlib
+import hashlib, time
 import re, os
 from shutil import rmtree
 from os import getcwd,walk
@@ -6,16 +6,46 @@ from os.path import join, normpath, relpath
 from subprocess import run
 from io import StringIO
 
-#check for remote version
-'''server.py should handle this
-The real time data base can store a version tag/integrity checksum'''
+last_download=0
+update_available=0
+current_version=0
+available_version=0
+download_complete=0
+checksum=0
+download_integity=0
+update_prompt=0
+reboot_prompt=0
 
-#download updates
+#########check for remote version########
+
+'''server.py handles this
+The real-time data-base stores a version tag and download-integrity checksum
+it changes update_available to a 1 when the version tag is different from
+the running apps current version'''
+
+##########download updates##########
+
+def update_folder_empty():
+    if any(os.scandir('version/update')):
+        return False
+    return True
 
 def get_update():
-    run(f"git clone https://github.com/TridosVilakroma/Pi-ro-safe.git version/update/Pi-ro-safe")
+    global last_download,download_complete
+    if update_folder_empty():
+        now=time.time()
+        if now>last_download+86400: #attempt once a day to perserve bandwidth
+            last_download=now
+            completed_download=run(f"git clone https://github.com/TridosVilakroma/Pi-ro-safe.git version/update/Pi-ro-safe")
+            if completed_download.returncode:
+                #non-zero return code indicates downloaod error
+                remove_update_data()
+                download_complete=0
+            else:
+                download_complete=1
 
-#check download integrity
+
+##########check download integrity##########
 
 def alpha_num_sort(iterable):
     """ Sort the given iterable in the way that humans expect."""
@@ -63,16 +93,31 @@ def generate_checksum():
         hash_md5.update(file_hash.encode('utf-8'))
     return hash_md5.hexdigest()
 
-#prompt user to update
+##########prompt user to update##########
 
-#update
+'''handled by main.py listen()
+it is observing update_prompt
+once update_prompt is set to 1 it pushes a message and
+activates a NotificationBadge on the msg_icon'''
+
+##########update##########
 
 def update():
-    run(f"git pull version/update/Pi-ro-safe main --allow-unrelated-histories")
+    completed_update=run(f"git pull version/update/Pi-ro-safe main --allow-unrelated-histories")
+    if completed_update.returncode:
+        #non-zero return code indicates update error
+        reboot_prompt=0
+    else:
+        reboot_prompt=1
 
-#prompt user to restart
+##########prompt user to restart##########
 
-#restart
+'''handled by main.py listen()
+it is observing reboot_prompt
+once reboot_prompt is set to 1 it pushes a message and
+activates a NotificationBadge on the msg_icon'''
+
+##########restart##########
 
 def  reboot():
     if os.name == 'nt':
@@ -80,7 +125,7 @@ def  reboot():
     if os.name == 'posix':
         run('sudo reboot now)')
 
-#delete update download
+##########delete update download##########
 
 def onerror(func, path, exc_info):
     """
@@ -102,4 +147,38 @@ def onerror(func, path, exc_info):
         raise
 
 def remove_update_data():
-    rmtree('version/update/Pi-ro-safe',ignore_errors=True,onerror=onerror)
+    try:
+        rmtree('version/update/Pi-ro-safe',ignore_errors=False,onerror=onerror)
+    except:
+        print('version/updater.py remove_update_data(): failed to remove all update data')
+
+##########updater logic service##########
+
+def update(*args):
+    global  update_prompt,checksum
+    if update_available:
+        get_update()
+    if update_folder_empty():
+        return
+    else:
+        if available_version:
+            try:
+                from version.version import version as UPDATE_VERSION
+                if available_version != UPDATE_VERSION:
+                    print('version/updater.py update(): downloaded update version does not match hosted version; removing update data')
+                    remove_update_data()
+            except ModuleNotFoundError:
+                print('version/updater.py update(): downloaded update version not found; removing update data')
+                remove_update_data()
+    if not checksum:
+        if download_complete:
+            checksum=generate_checksum()
+    if not all((checksum, download_integity, download_complete)):
+        return
+    if checksum == download_integity:
+        update_prompt=1
+    else:
+        update_prompt=0
+        checksum=0
+        print('version/updater.py update(): checksum does not match download_integrity; removing update data')
+        remove_update_data()
