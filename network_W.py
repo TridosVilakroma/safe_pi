fae1401a523535987959dcf7addccf6eb78b7926
CalebStock <calebstock91@gmail.com>
2023-06-23 14:54:31 -0500

    empty funcs added for nt testing

---
+++ network_W.py
@@ -0,0 +1,17 @@
+import subprocess
+
+
+def get_available():
+        return 'test\ntest2'
+
+def is_connected():
+        return False
+
+def get_ssid():
+        return ''
+
+def get_status():
+        return ''
+
+def get_signal():
+        return ''
