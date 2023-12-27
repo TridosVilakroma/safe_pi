435f2b77947e6353fa76ad4149635a78a68d7d6e
TridosVilakroma <calebstock91@gmail.com>
2023-12-27 12:10:34 -0600

    blue_relay_4 batch added

---
+++ device_classes/batches/blue_relay_4.py
@@ -0,0 +1,54 @@
+devices=[
+    {
+    "device_name": "Exhaust Fan",
+    "type": "Exfan",
+    "gpio_pin": 35,
+    "run_time": 0.0,
+    "color": [
+    0.0,
+    0.0,
+    0.0,
+    0.85
+    ],
+    "trigger": "low"
+    },
+    {
+    "device_name": "Makeup Air Fan",
+    "type": "MAU",
+    "gpio_pin": 37,
+    "run_time": 0,
+    "color": [
+    0.0,
+    0.0,
+    0.0,
+    0.85
+    ],
+    "trigger": "low"
+    },
+    {
+    "device_name": "Lights",
+    "type": "Light",
+    "gpio_pin": 38,
+    "run_time": 0,
+    "color": [
+    0.0,
+    0.0,
+    0.0,
+    0.85
+    ],
+    "trigger": "low"
+    },
+    {
+    "device_name": "Dry Contact",
+    "type": "Dry",
+    "gpio_pin": 40,
+    "run_time": 0,
+    "color": [
+    0.0,
+    0.0,
+    0.0,
+    0.85
+    ],
+    "trigger": "low"
+    }
+]
