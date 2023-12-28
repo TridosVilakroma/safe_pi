32f47eb81e07ce97083306aa23ba1445a77a8b75
CalebStock <calebstock91@gmail.com>
2023-12-27 19:36:58 -0600

    blue_relay_6 batch added

---
+++ device_classes/batches/blue_relay_6.py
@@ -0,0 +1,111 @@
+devices=[
+    {
+    "device_name": "Exhaust Fan 1",
+    "type": "Exfan",
+    "gpio_pin": 29,
+    "run_time": 0.0,
+    "color": [
+    0.0,
+    0.0,
+    0.0,
+    0.85
+    ],
+    "trigger": "low"
+    },
+    {
+    "device_name": "Exhaust Fan 2",
+    "type": "Exfan",
+    "gpio_pin": 31,
+    "run_time": 0.0,
+    "color": [
+    0.0,
+    0.0,
+    0.0,
+    0.85
+    ],
+    "trigger": "low"
+    },
+    {
+    "device_name": "Makeup Air Fan",
+    "type": "MAU",
+    "gpio_pin": 33,
+    "run_time": 0,
+    "color": [
+    0.0,
+    0.0,
+    0.0,
+    0.85
+    ],
+    "trigger": "low"
+    },
+    {
+    "device_name": "Lights",
+    "type": "Light",
+    "gpio_pin": 35,
+    "run_time": 0,
+    "color": [
+    0.0,
+    0.0,
+    0.0,
+    0.85
+    ],
+    "trigger": "low"
+    },
+    {
+    "device_name": "Dry Contact 1",
+    "type": "Dry",
+    "gpio_pin": 37,
+    "run_time": 0,
+    "color": [
+    0.0,
+    0.0,
+    0.0,
+    0.85
+    ],
+    "trigger": "low"
+    },
+    {
+    "device_name": "Dry Contact 2",
+    "type": "Dry",
+    "gpio_pin": 32,
+    "run_time": 0,
+    "color": [
+    0.0,
+    0.0,
+    0.0,
+    0.85
+    ],
+    "trigger": "low"
+    }
+]
+
+info=[
+'[size=18][u]Six Channel Blue Relay Board',
+'''Six devices will be added, all low trigger.
+The pins being used are 29,31,32,33,35, and 37.''',
+'[size=18][u]'+' '*80,
+'''Device: Exhaust Fan 1
+Pin: 29
+Trigger: Low
+Relay Ch: 1''',
+'''Device: Exhaust Fan 2
+Pin: 31
+Trigger: Low
+Relay Ch: 2''',
+'''Device: Makeup Air Fan
+Pin: 33
+Trigger: Low
+Relay Ch: 3''',
+'''Device: Lights
+Pin: 35
+Trigger: Low
+Relay Ch: 4''',
+'''Device: Dry Contact 1
+Pin: 37
+Trigger: Low
+Relay Ch: 5''',
+'''Device: Dry Contact 2
+Pin: 32
+Trigger: Low
+Relay Ch: 6'''
+]
