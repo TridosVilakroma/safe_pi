c00834c293fa6721836c91fff8b91d887aaedd21
TridosVilakroma <calebstock91@gmail.com>
2024-02-27 14:42:58 -0600

    added entry for pin trans menu

--- lang_dict.py
+++ lang_dict.py
@@ -154,6 +154,10 @@ DISABLE for 3 seconds.
  pending changes watermark? [/color][/size]''',
     'report_pending_confirm' : '[size=30][b][color=#000000] Continue [/color][/b][/size]',
     'report_pending_cancel' : '[size=30][b][color=#000000] Cancel [/color][/b][/size]',
+    'bcm_board_trans_overlay' : 'Translate Pin Numbering Mode',
+    'bcm_board_trans_text' : '[size=30][color=#000000]Translate all devices from broadcom\nmode into board mode?\n\nWarning: This will affect all devices.[/color][/size]',
+    'bcm_board_trans_confirm' : '[size=30][b][color=#000000] Translate [/color][/b][/size]',
+    'bcm_board_trans_cancel' : '[size=30][b][color=#000000] Cancel [/color][/b][/size]',
     'pending_watermark' : '[size=100][b][color=#787878] Pending Updates [/color][/b][/size]',
     'mount_overlay' : 'Mount Device',
     'mount_text' : '''[size=30][color=#000000] Insert external storage device 
