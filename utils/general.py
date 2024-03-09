f9157e92e402d3e3faaf83f61c7e776f3f9e4e44
CalebStock <calebstock91@gmail.com>
2024-03-08 18:07:41 -0600

    func added to efficiently load files in reverse order

--- utils/general.py
+++ utils/general.py
@@ -100,3 +100,32 @@ def pad_str(_string,_length):
     '''
     padding=_length-len(_string)
     return _string+' '*int(padding*2)
+
+def reverse_readline(filename, buf_size=8192):
+    """A generator that returns the lines of a file in reverse order"""
+    with open(filename, 'rb') as fh:
+        segment = None
+        offset = 0
+        fh.seek(0, os.SEEK_END)
+        file_size = remaining_size = fh.tell()
+        while remaining_size > 0:
+            offset = min(file_size, offset + buf_size)
+            fh.seek(file_size - offset)
+            buffer = fh.read(min(remaining_size, buf_size))
+            # remove file's last "\n" if it exists, only for the first buffer
+            if remaining_size == file_size and buffer[-1] == ord('\n'):
+                buffer = buffer[:-1]
+            remaining_size -= buf_size
+            lines = buffer.split('\n'.encode())
+            # append last chunk's segment to this chunk's last line
+            if segment is not None:
+                lines[-1] += segment
+            segment = lines[0]
+            lines = lines[1:]
+            # yield lines in this chunk except the segment
+            for line in reversed(lines):
+                # only decode on a parsed line, to avoid utf-8 decode error
+                yield line.decode()
+        # Don't yield None if the file was empty
+        if segment is not None:
+            yield segment.decode()
